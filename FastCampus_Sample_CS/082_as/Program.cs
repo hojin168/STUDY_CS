fcd0e6a9fadc76f01cf4776b11e4c80ea974acac
DESKTOP-B529Q6N\HHJ <zmfpdkep@naver.com>
2022-07-16 21:44:55 +0900

    #0716 강의코드

---
+++ FastCampus_Sample_CS/082_as/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _082_as
+{
+    class Base
+    {
+        int num;
+
+        public void Print()
+        {
+            Console.WriteLine("num: {0}", num);
+        }
+    }
+    
+    
+    class AA : Base
+    {
+        int a;
+
+        public void PrintA()
+        {
+            Console.WriteLine("a: {0}", a);
+        }
+    }
+
+
+    class BB : Base
+    {
+        int b;
+
+        public void PrintB()
+        {
+            Console.WriteLine("b: {0}", b);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Base _base = new Base();
+            _base.Print();
+
+            Base aa = new AA(); //부모클래스로 선언,  자식클래스로 객체를 만들수있음
+            aa.Print();
+            
+
+
+            
+            if (aa is BB)
+            {
+                Console.WriteLine("aa는 BB의 객체 입니다.");
+            }
+
+            else if (aa is AA)
+            {
+                Console.WriteLine("aa는 AA의 객체 입니다.");
+            }
+
+            Base bb = new BB();
+
+
+            //BB copyBB = (BB)bb;   1
+            BB copyBB = bb as BB; // 강제 형 변환
+            if(null != copyBB)
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("copyBB는 BB객체를 형식 변환!!");
+                copyBB.Print();
+            }
+
+            //AA copyAA = (AA)bb; //1   예외 상황 발생..  프로그램 다운
+            AA copyAA = bb as AA;
+            if (null == copyAA)
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("copyAA는 AA객체가 아니므로 null!!");
+             
+                copyAA = new AA();
+                copyAA.Print();
+                copyAA.PrintA();  // 내가 원하는 값이 아님
+
+                AA asAA = copyAA as AA; //강제 형 변환
+                asAA.PrintA();
+            }
+        }
+    }
+}
